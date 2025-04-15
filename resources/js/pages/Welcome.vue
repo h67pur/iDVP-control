a4f7cc87fa8b94d7d3fd122c3760e14d640f4111
h67pur <alexlunegov52@gmail.com>
2025-04-15 17:26:38 +0300

    Изменил описание iDVP

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -34,7 +34,7 @@ import { Head, Link } from '@inertiajs/vue3';
                 <div class="flex-1 rounded-bl-lg rounded-br-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] lg:rounded-br-none lg:rounded-tl-lg lg:p-20">
                     <h1 class="mb-4 text-2xl font-bold">Добро пожаловать в IDVP-Console</h1>
                     <p class="mb-6 text-[#706f6c] dark:text-[#A1A09A]">
-                        Interactive Data Verification Platform Console - передовое решение для автоматизированной
+                        Interactive Data Visualization Platform Console - передовое решение для автоматизированной
                         проверки и обработки данных.
                     </p>
 
@@ -73,7 +73,7 @@ import { Head, Link } from '@inertiajs/vue3';
                     <div class="absolute inset-0 flex flex-col items-center justify-center p-8 text-center">
                         <h1 class="text-7xl font-bold text-white">IDVP</h1>
                         <h2 class="text-7xl font-bold text-white">Console</h2>
-                        <p class="mt-4 text-lg text-white/80">Интерактивная платформа<br>верификации данных</p>
+                        <p class="mt-4 text-lg text-white/80">Интерактивная платформа<br>визуализации данных</p>
                     </div>
                     <div class="absolute inset-0 rounded-t-lg shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] lg:overflow-hidden lg:rounded-r-lg lg:rounded-t-none"></div>
                 </div>
